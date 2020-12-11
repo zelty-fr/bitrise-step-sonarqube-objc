0eb67ae75a79c008eab8942ab90587adf2ad2192
Marc Legay <marc@ru3.com>
2020-12-11 10:23:01 +0100

    prevent editing PR variables + add categories

--- step.sh
+++ step.sh
@@ -34,32 +34,15 @@ if [ ! -f $SONAR_SCANNER_CMD ]; then
   popd
 fi
 
+PR_ARGS=""
 if [[ -z "${BITRISE_PULL_REQUEST}" ]]; then
-  "${SONAR_SCANNER_CMD}" \
-   -Dsonar.projectKey="${sonar_project_key}" \
-   -Dsonar.organization="${sonar_orga_name}" \
-   -Dsonar.host.url="${sonar_endpoint}" \
-   -Dsonar.login="${sonar_token}" \
-   -Dsonar.sources=./ \
-   -Dsonar.cfamily.build-wrapper-output=./bw-output/ \
-   -Dsonar.pullrequest.branch=$BITRISE_GIT_BRANCH \
-   -Dsonar.pullrequest.base=$BITRISE_PR_BASE \
-   -Dsonar.pullrequest.key=$BITRISE_PULL_REQUEST
- else
-  "${SONAR_SCANNER_CMD}" \
-   -Dsonar.projectKey="${sonar_project_key}" \
-   -Dsonar.organization="${sonar_orga_name}" \
-   -Dsonar.host.url="${sonar_endpoint}" \
-   -Dsonar.login="${sonar_token}" \
-   -Dsonar.sources=./ \
-   -Dsonar.cfamily.build-wrapper-output=./bw-output/
- fi
-#else 
-#   "${SONAR_SCANNER_CMD}" \
-#   -Dsonar.projectKey="${sonar_project_key}" \
-#   -Dsonar.organization="${sonar_orga_name}" \
-#   -Dsonar.host.url="${sonar_endpoint}" \
-#   -Dsonar.login="${sonar_token}" \
-#   -Dsonar.sources=./ \
-#   -Dsonar.cfamily.build-wrapper-output=./bw_output/
-#fi
+  PR_ARGS="-Dsonar.pullrequest.branch=$BITRISE_GIT_BRANCH -Dsonar.pullrequest.base=$BITRISE_PR_BASE -Dsonar.pullrequest.key=$BITRISE_PULL_REQUEST"
+if
+
+"${SONAR_SCANNER_CMD}" \
+ -Dsonar.projectKey="${sonar_project_key}" \
+ -Dsonar.organization="${sonar_orga_name}" \
+ -Dsonar.host.url="${sonar_endpoint}" \
+ -Dsonar.login="${sonar_token}" \
+ -Dsonar.sources=./ \
+ -Dsonar.cfamily.build-wrapper-output=./bw-output/ ${PR_ARGS}
