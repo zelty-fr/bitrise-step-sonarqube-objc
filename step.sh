16453dfe47470e475392c1bd617bfb882a1d579d
Marc Legay <marc@ru3.com>
2020-12-11 10:41:48 +0100

    fix bash syntax error

--- step.sh
+++ step.sh
@@ -40,13 +40,16 @@ fi
 
 PR_ARGS=""
 if [[ -z "${BITRISE_PULL_REQUEST}" ]]; then
-  PR_ARGS="-Dsonar.pullrequest.branch=$BITRISE_GIT_BRANCH -Dsonar.pullrequest.base=$BITRISE_PR_BASE -Dsonar.pullrequest.key=$BITRISE_PULL_REQUEST"
-if
+  PR_ARGS="${PR_ARGS} -Dsonar.pullrequest.branch=${BITRISE_GIT_BRANCH} -Dsonar.pullrequest.key=${BITRISE_PULL_REQUEST}"
+  if [[ -z "${BITRISE_PR_BASE}" ]]; then
+    PR_ARGS="${PR_ARGS} -Dsonar.pullrequest.base=${BITRISE_PR_BASE}"
+  fi
+fi
 
-"${SONAR_SCANNER_CMD}" \
+"${SONAR_SCANNER_CMD}" ${PR_ARGS} \
  -Dsonar.projectKey="${sonar_project_key}" \
  -Dsonar.organization="${sonar_orga_name}" \
  -Dsonar.host.url="${sonar_endpoint}" \
  -Dsonar.login="${sonar_token}" \
  -Dsonar.sources=./ \
- -Dsonar.cfamily.build-wrapper-output=./bw-output/ ${PR_ARGS}
+ -Dsonar.cfamily.build-wrapper-output=./bw-output/
