17ac128131423b229423dc1deeccbbebf59f695d
Marc Legay <marc@zelty.fr>
2021-08-13 18:13:15 +0200

    fix: path

--- step.sh
+++ step.sh
@@ -5,6 +5,8 @@ echo "BITRISE_GIT_BRANCH=${BITRISE_GIT_BRANCH}"
 echo "BITRISE_PULL_REQUEST=${BITRISE_PULL_REQUEST}"
 echo "BITRISEIO_GIT_BRANCH_DEST=${BITRISEIO_GIT_BRANCH_DEST}"
 
+BUILD_DIR=/Users/vagrant/Build
+
 JAVA_VERSION_MAJOR=$(java -version 2>&1 | grep -i version | sed 's/.*version ".*\.\(.*\)\..*"/\1/; 1q')
 if [ ! -z "${JAVA_VERSION_MAJOR}" ]; then
   if [ "${JAVA_VERSION_MAJOR}" -lt "8" ]; then
@@ -14,33 +16,33 @@ else
   echo -e "\e[91mSonar Scanner CLI requires JRE or JDK version 8 or newer. None has been detected, CLI may not work properly.\e[0m"
 fi
 
-SONAR_WARPPER_CMD=$BITRISE_SOURCE_DIR/build-wrapper-macosx-x86/build-wrapper-macosx-x86
+SONAR_WARPPER_CMD=$BUILD_DIR/build-wrapper-macosx-x86/build-wrapper-macosx-x86
 if [ ! -f $SONAR_WARPPER_CMD ]; then
-  pushd $BITRISE_SOURCE_DIR
+  pushd $BUILD_DIR
   wget https://sonarcloud.io/static/cpp/build-wrapper-macosx-x86.zip
   unzip build-wrapper-macosx-x86.zip
   popd
 fi
 
 env NSUnbufferedIO=YES \
-"${SONAR_WARPPER_CMD}" --out-dir bw-output xcodebuild \
+"${SONAR_WARPPER_CMD}" --out-dir $BUILD_DIR/bw-output xcodebuild \
 -workspace "${xcodebuild_workspace}" \
 -scheme "${xcodebuild_scheme}" \
 -destination "${xcodebuild_destination}" \
--derivedDataPath derived_data_path \
+-derivedDataPath $BUILD_DIR/derived_data_path \
 ${xcodebuild_actions} CODE_SIGNING_REQUIRED=NO COMPILER_INDEX_STORE_ENABLE=NO | xcpretty
 
-SONAR_XCCOV_CMD=$BITRISE_SOURCE_DIR/xccov-to-sonarqub-generic.sh
+SONAR_XCCOV_CMD=$BUILD_DIR/xccov-to-sonarqub-generic.sh
 if [ ! -f $SONAR_XCCOV_CMD ]; then
-	pushd $BITRISE_SOURCE_DIR
+	pushd $BUILD_DIR
 	wget https://raw.githubusercontent.com/SonarSource/sonar-scanning-examples/master/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh -O $SONAR_XCCOV_CMD
 	popd
 fi
-bash $SONAR_XCCOV_CMD derived_data_path/Logs/Test/*.xcresult/ > sonarqube-generic-coverage.xml
+bash $SONAR_XCCOV_CMD $BUILD_DIR/derived_data_path/Logs/Test/*.xcresult/ > $BUILD_DIR/sonarqube-generic-coverage.xml
 
-SONAR_SCANNER_CMD=$BITRISE_SOURCE_DIR/sonar-scanner-${scanner_version}/bin/sonar-scanner
+SONAR_SCANNER_CMD=$BUILD_DIR/sonar-scanner-${scanner_version}/bin/sonar-scanner
 if [ ! -f $SONAR_SCANNER_CMD ]; then
-  pushd $BITRISE_SOURCE_DIR
+  pushd $BUILD_DIR
   wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${scanner_version}.zip
   unzip sonar-scanner-cli-${scanner_version}.zip
   popd
@@ -64,5 +66,5 @@ fi
  -Dsonar.host.url="${sonar_endpoint}" \
  -Dsonar.login="${sonar_token}" \
  -Dsonar.sources=./ \
- -Dsonar.coverageReportPaths=sonarqube-generic-coverage.xml \
- -Dsonar.cfamily.build-wrapper-output=./bw-output/
+ -Dsonar.coverageReportPaths=$BUILD_DIR/sonarqube-generic-coverage.xml \
+ -Dsonar.cfamily.build-wrapper-output=$BUILD_DIR/bw-output/
